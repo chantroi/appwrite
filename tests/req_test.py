c26e988d90df96685bc1bda08c456fe5abfd379f
chantroi <tonglanhchua@gmail.com>
2024-02-18 13:39:55 +0700

    update tests/req_test.py

--- tests/req_test.py
+++ tests/req_test.py
@@ -2,16 +2,16 @@ import json
 
 def main(context):
     response = "RAW BODY: {}\n\nJSON: {}\n\nHEADERS: {}\n\nSCHEME: {}\n\nMETHOD: {}\n\nURL: {}\n\nHOST: {}\n\nPORT: {}\n\nPATH: {}\n\nQUERY: {}\n\nJSON QUERY: {}".format(
-    context.req.body_raw,
-    json.dumps(context.req.body),
-    json.dumps(context.req.headers),
-    context.req.scheme,
-    context.req.method,
-    context.req.url,
-    context.req.host,
-    context.req.port,
-    context.req.path,
-    context.req.query_string,
-    json.dumps(context.req.query)
+        context.req.body_raw,
+        json.dumps(context.req.body),
+        json.dumps(context.req.headers),
+        context.req.scheme,
+        context.req.method,
+        context.req.url,
+        context.req.host,
+        context.req.port,
+        context.req.path,
+        context.req.query_string,
+        json.dumps(context.req.query)
     )
     return context.res.send(response, 200, {"Content-Type": "text/plain"})
