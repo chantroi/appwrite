329a35e5748e95c4125768804dcd6f72e092d5e6
chantroi <tonglanhchua@gmail.com>
2024-02-18 19:35:25 +0700

    update tests/req_test.py

--- tests/req_test.py
+++ tests/req_test.py
@@ -3,8 +3,8 @@ import json
 def main(context):
     response = "RAW BODY: {}\n\nJSON: {}\n\nHEADERS: {}\n\nSCHEME: {}\n\nMETHOD: {}\n\nURL: {}\n\nHOST: {}\n\nPORT: {}\n\nPATH: {}\n\nQUERY: {}\n\nJSON QUERY: {}".format(
         context.req.body_raw,
-        json.dumps(context.req.body),
-        json.dumps(context.req.headers),
+        context.req.body,
+        context.req.headers,
         context.req.scheme,
         context.req.method,
         context.req.url,
