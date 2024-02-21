e42bc0acd4a06201e5c0cefda7f2389bd3850429
chantroi <tonglanhchua@gmail.com>
2024-02-21 08:44:51 +0700

    update telegram/main.py

--- telegram/main.py
+++ telegram/main.py
@@ -1,10 +1,12 @@
 import json
 import requests
+from types import SimpleNamespace
 
 def main(context):
-    data = context.req.body_raw
+    data = context.req.body
+    data = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
     context.log(data)
     bot = context.req.query.get("bot")
-    res = requests.get(f"https://api.telegram.org/bot{bot}/sendMessage", params={"text": f"```json {data}```", "chat_id": 5665225938, "parse_mode": "markdown"})
+    res = requests.get(f"https://api.telegram.org/bot{bot}/sendMessage", params={"text": f"```json {data}```", "chat_id": data.message.chat.id, "parse_mode": "markdown"})
     context.log(res.text)
     return context.res.send(data)
