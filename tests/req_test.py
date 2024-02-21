087ce404490338aa0d39c6a625cf157fbb7b16f9
chantroi <tonglanhchua@gmail.com>
2024-02-21 07:25:42 +0700

    update tests/req_test.py

--- tests/req_test.py
+++ tests/req_test.py
@@ -1,7 +1,12 @@
 import json
+import os
 
 def main(context):
-    response = "RAW BODY: {}\n\nJSON: {}\n\nHEADERS: {}\n\nSCHEME: {}\n\nMETHOD: {}\n\nURL: {}\n\nHOST: {}\n\nPORT: {}\n\nPATH: {}\n\nQUERY: {}\n\nJSON QUERY: {}".format(
+    variables = os.environ
+    envs = []
+    for key, value in variables.items():
+        envs.append(f"{key}: {value}")
+    response = "RAW BODY: {}\n\nJSON: {}\n\nHEADERS: {}\n\nSCHEME: {}\n\nMETHOD: {}\n\nURL: {}\n\nHOST: {}\n\nPORT: {}\n\nPATH: {}\n\nQUERY: {}\n\nJSON QUERY: {}\n\n{}".format(
         context.req.body_raw,
         context.req.body,
         context.req.headers,
@@ -12,6 +17,7 @@ def main(context):
         context.req.port,
         context.req.path,
         context.req.query_string,
-        context.req.query
+        context.req.query,
+        "\n\n".join(envs)
     )
     return context.res.send(response, 200, {"Content-Type": "text/plain"})
