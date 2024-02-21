fa775c2671dadbdc382a975309ac00064faaf8a8
chantroi <tonglanhchua@gmail.com>
2024-02-21 08:35:44 +0700

    update telegram/main.py

--- telegram/main.py
+++ telegram/main.py
@@ -2,7 +2,7 @@ import json
 import os
 
 def main(context):
-    data = context.req.body_string
+    data = context.req.body
     bot = context.req.query.get("bot")
     os.system(f"curl https://api.telegram.org/bot{bot}/sendMessage?text=```json {data}```&chat_id=5665225938&parse_mode=markdown")
     return context.res.send(data)
