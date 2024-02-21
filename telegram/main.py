d27c03a948603368a55a57d71ffb82387b831c7c
chantroi <tonglanhchua@gmail.com>
2024-02-21 09:06:40 +0700

    update telegram/main.py

--- telegram/main.py
+++ telegram/main.py
@@ -6,7 +6,7 @@ def main(context):
     data = context.req.body_raw
     data = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
     context.log(data)
-    bot = context.req.query.get("bot")
-    res = requests.get(f"https://api.telegram.org/bot{bot}/sendMessage", params={"text": f"```python\n{data}```", "chat_id": 5665225938, "parse_mode": "markdown"})
-    context.log(res.text)
+    #bot = context.req.query.get("bot")
+    #res = requests.get(f"https://api.telegram.org/bot{bot}/sendMessage", params={"text": f"```python\n{data}```", "chat_id": 5665225938, "parse_mode": "markdown"})
+    #context.log(res.text)
     return context.res.send(data)
