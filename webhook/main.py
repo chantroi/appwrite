31350c1a05e01967f9f5da3c5b9caf76c46a208f
chantroi <tonglanhchua@gmail.com>
2024-02-24 18:08:13 +0700

    update webhook/main.py

---
+++ webhook/main.py
@@ -0,0 +1,13 @@
+import os
+import json
+import requests
+from types import SimpleNamespace
+
+def main(ctx):
+    value = ctx.req.body_raw
+    obj = json.loads(value, object_hook=lambda d: SimpleNamespace(**d))
+    jobj = json.loads(value)
+    bot_api = "https://api.telegram.org/bot" + os.getenv('TG_BOT')
+    data = dict(chat_id=os.getenv('TG_CHAT'), text=f"```json\n{jobj}```")
+    r = requests.post(bot_api + "/sendMessage", json=data)
+    return ctx.res.json(r.json())
