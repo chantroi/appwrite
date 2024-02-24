6150fd6dba5e601cf0d3259d48e57ee13871d05c
chantroi <tonglanhchua@gmail.com>
2024-02-24 18:14:28 +0700

    update webhook/main.py

--- webhook/main.py
+++ webhook/main.py
@@ -10,6 +10,6 @@ def main(ctx):
     bot_token = ctx.req.query.get("bot")
     chat = ctx.req.query.get("chat")
     bot_api = "https://api.telegram.org/bot" + bot_token
-    data = dict(chat_id=chat, text=f"```json\n{jobj}```")
+    data = dict(chat_id=chat, text=f"```json\n{jobj}```", parse_mode="markdown")
     r = requests.post(bot_api + "/sendMessage", json=data)
     return ctx.res.json(r.json())
